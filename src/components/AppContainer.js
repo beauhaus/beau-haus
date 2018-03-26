839ae0cda7a04153f2b4eba1e31cbac13dbcc5cf
beauvelop <beau.html@gmail.com>
2018-03-26 08:36:16 -0400

    reflection achieved

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -6,13 +6,12 @@ import BackTree from './LandingTree/BackTree';
 import MidTree from './LandingTree/MidTree';
 import FrontTree from './LandingTree/FrontTree';
 
-
 class Background extends React.Component {
   state = {
     options: [],
-    offsetMatch: "#a8a8a8",
-    bgRight: "#fff",
-    pondForeground: "#cfcfcf"
+    offsetMatch: '#a8a8a8',
+    bgRight: '#fff',
+    pondForeground: '#cfcfcf'
   };
 
   render() {
@@ -20,45 +19,85 @@ class Background extends React.Component {
     return (
       <svg viewBox="0 0 1920 1080" preserveAspectRatio="none">
         <linearGradient id="bg-grad">
-        <stop className="offset-match" offset="0" stopColor={this.state.offsetMatch} />
-        <stop className="bg-right" offset="1" stopColor={this.state.bgRight} />
+          <stop className="offset-match" offset="0" stopColor={this.state.offsetMatch} />
+          <stop className="bg-right" offset="1" stopColor={this.state.bgRight} />
         </linearGradient>
         <path fill="url(#bg-grad)" d="M.5 0h1920v1080H.5z" />
-        <Pond foregroundColor={this.state.pondForeground} offset={this.state.offsetMatch}/>
+        <Pond foregroundColor={this.state.pondForeground} offset={this.state.offsetMatch} />
       </svg>
     );
   }
 }
 
+
+/** Do away with viewBox Create new SVG exactly the size of this component
+ * and do a transform translate to the image.
+ * Do proper math
+ */
 class Preview extends React.Component {
   state = {
     thumbnail: []
-  }
+  };
   render() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080">
-    <g id="display">
-      <polygon id="reflection" fill="#73FFFF" points="1614.5 993 850.5 993 840.5 880 1623.5 880"/>
-      <rect id="view" width="784" height="441" x="839.5" y="439" fill="#FF7BAC"/>
-    </g>
-</svg>
+      <svg id="brdr1" xmlns="http://www.w3.org/2000/svg" viewBox="300 0 1920 1080">
+        <defs>
+          <linearGradient opacity=".5" id="grad" x1="0%" y1="10%" x2="10%" y2="100%">
+            <stop stopColor="transparent" offset="0" />
+            <stop stopColor="white" stopOpacity="1" offset=".9" />
+          </linearGradient>
+          <mask id="mask1">
+            <polygon fill="url(#grad)" points="1623 993 841 993 850.5 880 1613.5 880" />
+          </mask>
+          <pattern id="pattern1" height="100%" width="100%" patternContentUnits="objectBoundingBox">
+            <image  height="1" width="1"  preserveAspectRatio="none" href="https://dl.dropbox.com/s/f6vtd4o0vxx1378/ostrich.jpg?dl=0"/>
+        </pattern>
+        </defs>
 
-    )
+        <g id="display">
+          <rect id="view" width="784" height="441" x="839.5" y="439" fill="url(#pattern1)"/>
+          <polygon
+            id="reflection"
+            mask="url(#mask1)"
+            fill="url(#pattern1)"
+            points="1623 993 841 993 850.5 880 1613.5 880"
+          />
+        </g>
+      </svg>
+    );
   }
 }
 
+/*
+//mask and polygon
+points="1614.5 993 850.5 993 840.5 880 1623.5 880"
+
+<g id="display">
+      <polygon id="reflection" fill="#73FFFF" points="1614.5 993 850.5 993 840.5 880 1623.5 880"/>
+      <rect id="view" width="784" height="441" x="839.5" y="439" fill="#FF7BAC"/>
+    </g>
+<defs>
+    <linearGradient id="grad" x1="0%" y1="10%" x2="10%" y2="100%">
+      <stop stop-color="white" offset="0"/>
+      <stop stop-color="transparent" stop-opacity="1" offset=".9"/>
+    </linearGradient>
+      <mask id="mask1">
+        <rect width="400" height="400" fill="url(#grad)"/>
+    </mask>
+    </defs>
+  <image  width="300" height="300" y="0" x="0" mask="url(#mask1)" xlink:href="https://dl.dropbox.com/s/f6vtd4o0vxx1378/ostrich.jpg?dl=0"/>
+  */
+
 class AppContainer extends React.Component {
   state = {
-    options: [],
+    options: []
   };
 
   render() {
-
-
     return (
       <div className="svg-container">
         <Background />
-        <Preview/>
+        <Preview />
         <h1 className="beauhaus-banner">BEAUHAUS</h1>
         <BackTree />
         <MidTree />
