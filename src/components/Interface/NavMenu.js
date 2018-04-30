8a57c0905f3fca0aff3c7c310db5899ffb341c1b
BEAUHAUS <beau.html@gmail.com>
2018-04-30 04:35:51 -0400

    a cleanuer MenuNav--prep to animate

--- src/components/Interface/NavMenu.js
+++ src/components/Interface/NavMenu.js
@@ -14,8 +14,42 @@ class NavMenu extends React.Component {
       page: props.palette
     };
     // this.handleClick = this.handleClick.bind(this);
+    this.renderBtns = this.renderBtns.bind(this);
   }
 
+  renderBtns(btn, idx) {
+    const { palette } = this.props;
+    // const btns = BtnDB.btns;
+    return (
+      <Link key={btn.id + 1} 
+      onClick={() => this.props.click(palette[idx].color)}
+      to={`/${btn.name!=="home"? btn.name: ''}`}>
+        <text key={btn.id} transform={btn.txtPos}>
+          {' '}
+          {btn.name}{' '}
+        </text>
+        <circle
+          key={btn.id + 2}
+          transform={this.state.btnPos}
+          className="btn btn-menu-light"
+          id={`btn-${btn.name}`}
+          cx={btn.x}
+          cy={btn.y}
+          r={btn.r}
+          fill={palette[idx].color}
+        />
+        <circle
+          key={btn.id + 3}
+          transform={this.state.btnPos}
+          className="btn btn-menu-mute"
+          cx={btn.x}
+          cy={btn.y}
+          r={btn.r}
+          fill={palette[idx].color}
+        />
+      </Link>
+    );
+  }
   render() {
     const { palette } = this.props;
     // console.log(props)
@@ -24,38 +58,11 @@ class NavMenu extends React.Component {
     // console.log(props)
     return (
       <div className="nav-menu">
-        <svg id="menu-desktop" viewBox="0 0 400 500" preserveAspectRatio="xMidYMid meet">
+        <svg id="menu-desktop" viewBox="76.5 0 479 500" preserveAspectRatio="xMidYMid meet">
           <filter id="filter-saturate">
             <feColorMatrix type="saturate" values=".2" />
           </filter>
-
-          {BtnDB.btns.map((btn, idx) => (
-            <Link key={btn.id + 1} to={`/${btn.name!=="home"? btn.name: ''}`}>
-              <circle
-                key={btn.id + 2}
-                transform={this.state.btnPos}
-                className="btn btn-menu-light"
-                id={`btn-${btn.name}`}
-                cx={btn.x}
-                cy={btn.y}
-                r={btn.r}
-                fill={palette[idx].color}
-              />
-              <circle
-                onClick={() => this.props.click(palette[idx].color)}
-                key={btn.id + 3}
-                transform={this.state.btnPos}
-                className="btn btn-menu-mute"
-                cx={btn.x}
-                cy={btn.y}
-                r={btn.r}
-                fill={palette[idx].color}
-              />
-              <text onClick={() => this.props.click(btn.name)} key={btn.id + 4} transform={btn.txtPos}>
-                {btn.name}
-              </text>
-            </Link>
-          ))}
+          {BtnDB.btns.map(this.renderBtns)}
         </svg>
       </div>
     );
