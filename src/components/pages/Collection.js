a950f688822c19c816ebec3d0d2fcce8bf80fe1b
BEAUHAUS <beau.html@gmail.com>
2018-06-05 11:11:51 -0400

    adds props to banner & directory tree cleanup

--- src/components/pages/Collection.js
+++ src/components/pages/Collection.js
@@ -11,6 +11,9 @@ import Shore from '../scene/water/Shore';
 import MuteBtn from '../scene/MuteBtn';
 import LogoTheme from '../scene/LogoTheme';
 
+import LinksIcon from './CollectionComps/LinksIcon';
+import PageBanner from './CollectionComps/CollectionPageBanner';
+
 import Slideshow from '../UI/Slideshow';
 import SlidesData from '../../data/SlideShowData.json';
 /* grid is fibonacci @WIDTH 51vw  X 31.5vh (1.5 * 34) X (1.5 X 21)
@@ -28,7 +31,7 @@ const CollectionPageCompDiv = styled.div`
   /* customized size of grid using fr to do fibonacci calculations */
   grid-template-columns: 20vw 60vw 20vw;
   grid-template-rows: 35vh 45vh 20vh;
-  & > div.topic-container {
+  & > div.page-banner {
     grid-column: 3;
     grid-row: 1/4;
     position: relative;
@@ -222,64 +225,7 @@ const CollectionPageCompDiv = styled.div`
   }
 `;
 
-const LinkIconSVG = styled.svg`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-`;
-const LinkIcon = props => {
-  return (
-    <svg viewBox="0 0 77 59" preserveAspectRatio="none">
-      <linearGradient
-        id="a"
-        x1="28.3789"
-        x2="28.3789"
-        y1="37.3262"
-        y2="14.0533"
-        gradientTransform="rotate(-34.694 42.1853408 18.33635557)"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#799493" />
-        <stop offset="1" stopColor="#283030" />
-      </linearGradient>
-      <path
-        fill="url(#a)"
-        d="M66.1 11.8c3.6 4.8 2.2 11.3-3 14.7L19.4 54.4c-5.3 3.4-12.4 2.2-16-2.5-3.6-4.8-2.2-11.3 3-14.7l43.7-28c5.2-3.3 12.4-2.2 16 2.6z"
-      />
-      <linearGradient
-        id="b"
-        x1="374.4551"
-        x2="374.4551"
-        y1="-150.9268"
-        y2="-173.6953"
-        gradientTransform="rotate(145.306 183.37515186 -121.07642606)"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#945765" />
-        <stop offset=".9957" stopColor="#240008" />
-      </linearGradient>
-      <path
-        fill="url(#b)"
-        d="M26.8 36.8c-3.6-4.8-3.3-10.6.6-13.1L59.7 3.1c3.9-2.5 10-.6 13.5 4.1 3.6 4.8 3.3 10.6-.6 13.1L40.4 41c-3.9 2.4-10 .6-13.6-4.2z"
-      />
-      <linearGradient
-        id="c"
-        x1="18.6953"
-        x2="18.6953"
-        y1="36.5166"
-        y2="14.5469"
-        gradientTransform="rotate(-34.694 42.1853408 18.33635557)"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#799493" />
-        <stop offset="1" stopColor="#283030" />
-      </linearGradient>
-      <path fill="url(#c)" d="M16.9 56l32.3-20.6c3.9-2.5 4.1-8.4.6-13.1L3.3 51.8c3.6 4.8 9.7 6.6 13.6 4.2z" />
-    </svg>
-  );
-};
+
 class Collection extends Component {
   constructor(props) {
     super(props);
@@ -292,7 +238,7 @@ class Collection extends Component {
     };
     this.clickHandler = this.clickHandler.bind(this);
     // this.changeHandler = this.changeHandler.bind(this);
-    // console.log('props>Collection: ', props);
+    console.log('props>Collection: ', props);
   }
   componentDidMount() {
     // console.log("this.state.slides.length", this.state.total)
@@ -308,15 +254,13 @@ class Collection extends Component {
 
   render() {
     const { pageStyles, tree, water } = this.props.profile;
-    const { fill } = this.props.profile.pageStyles.fill;
+    const { fill } = this.props.profile.pageStyles;
     const currentslide = this.state.slides[this.state.current];
     // console.log('currentslide', currentslide);
     const { proj_icon, proj_number, proj_title, proj_tech, proj_desc, proj_links } = currentslide;
     return (
       <CollectionPageCompDiv className="page collection-container" style={pageStyles}>
-        <div className="topic-container">
-          <img src="./img/pagesimg/collectionbanner.svg" alt="collection banner" />
-        </div>
+       <PageBanner fill={fill}/>
         <div className="grid-ctr-tall fading-in">
           
 
@@ -338,7 +282,7 @@ class Collection extends Component {
           <section>
             <div className="meta-data-nums" />
             <div className="meta-data-links">
-              <LinkIcon />
+              <LinksIcon />
               <ul>
                 {proj_links.map((item, idx) => (
                   <li key={`${item}-${idx}li`}>
